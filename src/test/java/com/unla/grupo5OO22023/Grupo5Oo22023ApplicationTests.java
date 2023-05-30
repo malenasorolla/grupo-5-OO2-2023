a84d9f2523df0cf0054f9024b52e81daf34a9c89
Miguel Caraballo <mecf@Miguelecf.local>
2023-05-30 16:37:49 -0300

    probando el primer commmit del proyecto

---
+++ src/test/java/com/unla/grupo5OO22023/Grupo5Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo5OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
