d671eca616b7f14cdcc9d8ffa9412c43d3a7c8d0
Miguel Caraballo <mecf@Miguelecf.local>
2023-06-23 23:26:32 -0300

    Arreglando backend e impactando en el front

--- src/main/java/com/unla/grupo5OO22023/controllers/DispositivoController.java
+++ src/main/java/com/unla/grupo5OO22023/controllers/DispositivoController.java
@@ -12,10 +12,12 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.unla.grupo5OO22023.entity.Dispositivo;
 import com.unla.grupo5OO22023.entity.SensorLuz;
+import com.unla.grupo5OO22023.entity.SensorProximidad;
 import com.unla.grupo5OO22023.helpers.ViewRouteHelper;
 import com.unla.grupo5OO22023.models.DispositivoModel;
 import com.unla.grupo5OO22023.services.IDispositivoService;
 import com.unla.grupo5OO22023.services.ISensorLuzService;
+import com.unla.grupo5OO22023.services.ISensorProximidadService;
 
 @Controller
 @RequestMapping("/dispositivo")
@@ -28,33 +30,51 @@ public class DispositivoController {
 	@Qualifier("sensorluzService")
 	private ISensorLuzService sensorluzService;
 
-	
-    @GetMapping("/listar")
-    public String index(Model model){
-       model.addAttribute("dispositivos", dispositivoService.getAll());
-       return ViewRouteHelper.DispositivoIndex;
-    }
+	@Autowired
+	@Qualifier("sensorProximidadService")
+	private ISensorProximidadService sensorProximidadService;
 
-    @PostMapping("/editar")
-    public RedirectView create(@ModelAttribute("dispositivo") DispositivoModel dispositivoModel){
-        dispositivoService.insertOrUpdate(dispositivoModel);
-        return new RedirectView(ViewRouteHelper.DispositivoRuta);
-    }
-    
-    // ************************SENSORLUZ***********************************************************
-    
-    @GetMapping("/newsluz")
-    public String agregar(Model model) {
-    	SensorLuz sensorluz = new SensorLuz();
-    	model.addAttribute("sensorluz",sensorluz);
-    	return ViewRouteHelper.FORM_SENSORLUZ;
-    }
-    
-    @PostMapping("/savesluz")
-    public String save(Dispositivo dispositivo, Model model) {
-    	sensorluzService.save((SensorLuz)(dispositivo));
-    	return "redirect:/listar";
-    }
-    
-    
+	@GetMapping("/listar")
+	public String index(Model model) {
+		model.addAttribute("dispositivos", dispositivoService.getAll());
+		return ViewRouteHelper.DispositivoIndex;
+	}
+
+	@PostMapping("/editar")
+	public RedirectView create(@ModelAttribute("dispositivo") DispositivoModel dispositivoModel) {
+		dispositivoService.insertOrUpdate(dispositivoModel);
+		return new RedirectView(ViewRouteHelper.DispositivoRuta);
+	}
+
+	// ************************SENSORLUZ***********************************************************
+
+	@GetMapping("/newsluz")
+	public String agregar(Model model) {
+		SensorLuz sensorluz = new SensorLuz();
+		model.addAttribute("sensorluz", sensorluz);
+		return ViewRouteHelper.FORM_SENSORLUZ;
+	}
+
+	@PostMapping("/savesluz")
+	public String save(Dispositivo dispositivo, Model model) {
+		sensorluzService.save((SensorLuz) (dispositivo));
+		return "redirect:/listar";
+	}
+
+	// ************************SENSORPROXIMIDAD***********************************************************
+
+	@GetMapping("/newsproxi")
+	public String agregarProximidad(Model model) {
+		SensorProximidad sensorProximidad = new SensorProximidad();
+//    	model.addAttribute("sensorProximidad",sensorProximidad);
+		model.addAttribute("sensorProximidad", sensorProximidad);
+		return ViewRouteHelper.FORM_SENSORPROXIMIDAD;
+	}
+
+	@PostMapping("/savesproxi")
+	public String saveProximidad(Dispositivo dispositivo, Model model) {
+		sensorProximidadService.save((SensorProximidad) (dispositivo));
+		return "redirect:/listar";
+	}
+
 }
